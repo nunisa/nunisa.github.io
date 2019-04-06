9970495c3628a7b34aa4d4799053fb9986e97ae2
Monjit Nunisa <monjitnunisa@Monjits-MacBook-Pro.local>
2019-04-06 22:25:36 +0530

    First blog's commit

---
+++ precache-manifest.96857b8e7317a8bf26a23e698bae8d70.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "7414163434ddcb2d908c0f155bccadf8",
+    "url": "/static/media/create_hosted_zone_pane.74141634.png"
+  },
+  {
+    "revision": "3755e8b254eda857ad3f",
+    "url": "/static/css/main.bf48b619.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d953737e52c44a328d8",
+    "url": "/static/js/2.a93a8cb7.chunk.js"
+  },
+  {
+    "revision": "747b7f03d0d95975a0afb4aeb65c36a9",
+    "url": "/static/media/logo.747b7f03.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f39373d64e6af544164149eac016157",
+    "url": "/static/media/advanced_features.1f39373d.png"
+  },
+  {
+    "revision": "2028dc276a8161003411577efd377c45",
+    "url": "/static/media/bucket_policy.2028dc27.png"
+  },
+  {
+    "revision": "bcd3543ad50266c8339e8cf82f6e445a",
+    "url": "/static/media/bucket_properties.bcd3543a.png"
+  },
+  {
+    "revision": "8f20b9ea4533918abb7b08f3348713f2",
+    "url": "/static/media/create_bucket.8f20b9ea.png"
+  },
+  {
+    "revision": "59bb83617489dcba486e2db5a92bbf8f",
+    "url": "/static/media/create_bucket_dialog.59bb8361.png"
+  },
+  {
+    "revision": "5dce48bd2bb87e7cbd3f46080221991e",
+    "url": "/static/media/create_bucket_permissions.5dce48bd.png"
+  },
+  {
+    "revision": "462810c2caa6bedce36c520a125f4433",
+    "url": "/static/media/create_bucket_review.462810c2.png"
+  },
+  {
+    "revision": "82b4275148850e85182029ff1babe757",
+    "url": "/static/media/create_hosted_zone.82b42751.png"
+  },
+  {
+    "revision": "3755e8b254eda857ad3f",
+    "url": "/static/js/main.13c6e453.chunk.js"
+  },
+  {
+    "revision": "de6a8c3fcc44b3968572fffefb27bcbd",
+    "url": "/static/media/custom_nameservers.de6a8c3f.png"
+  },
+  {
+    "revision": "2cc43c09fc0e8fd8c2777ea872345eb8",
+    "url": "/static/media/edit_typeA_record.2cc43c09.png"
+  },
+  {
+    "revision": "e3dec82c2dcc8a11f716f1e5b614b6b4",
+    "url": "/static/media/godaddy_custom.e3dec82c.png"
+  },
+  {
+    "revision": "aff2722149a2dd96f9c09250416f7af9",
+    "url": "/static/media/godaddy_nameservers.aff27221.png"
+  },
+  {
+    "revision": "bd5fe5cf76dec43fe43919dbc426c5ff",
+    "url": "/static/media/hosted_zone_nameservers.bd5fe5cf.png"
+  },
+  {
+    "revision": "1c26f25f64d3079cbf1cfae8252fba47",
+    "url": "/static/media/manage_dns.1c26f25f.png"
+  },
+  {
+    "revision": "c8a45ac237f750d7cc2dce318e654edd",
+    "url": "/static/media/ns_soa_records.c8a45ac2.png"
+  },
+  {
+    "revision": "888dc0d9ddc7d528060f0e7bf55eaa2c",
+    "url": "/static/media/refresh_records.888dc0d9.png"
+  },
+  {
+    "revision": "0645f6102445c89d4b1c13537b7ff6f1",
+    "url": "/static/media/route53_get_started.0645f610.png"
+  },
+  {
+    "revision": "7767d5e893145931be8a40c483ef05d9",
+    "url": "/static/media/search_route53.7767d5e8.png"
+  },
+  {
+    "revision": "22ba40bb62710df0cbc76bd5931ebbcb",
+    "url": "/static/media/search_s3.22ba40bb.png"
+  },
+  {
+    "revision": "493f200c8de922132efb3fe201af8a16",
+    "url": "/static/media/sw_upload_files.493f200c.png"
+  },
+  {
+    "revision": "bc6d909f94770b400bbca3d5d705d54e",
+    "url": "/index.html"
+  }
+];
